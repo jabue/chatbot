b0bfdd619cdc517362034b187d1c0b6afa420088
Ruifeng Liu <ruifengliu@Ruifengs-MacBook-Air.local>
2025-09-11 21:41:05 -0400

    Added a module named chatbot

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ChatbotModule } from './chatbot.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ChatbotModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
